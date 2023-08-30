36126716306ac79185272fb1549a6eaca41ae781
Nyaundi <jamesmabuka260@gmail.com>
2023-08-30 16:40:35 +0300

    Js Failed so I had to comment it out

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 //queryselectorall()
 
-const listItems = document.querySelectorAll('.items');
-console.log(listItems);
+// const listItems = document.querySelectorAll('.item');
+// console.log(listItems[1].innerText);
+
+// listItems[1].style.color = 'red';
+
+// listItems.forEach((item, index) => {
+//     item.style.color = 'red';
+
+//     // if (index === 1) {
+//     //     item.remove();
+//     // }
+
+//     if (index === 0 ){
+//         item.innerHTML = `Avocadoes
+//         <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//         </button>`;
+//     }
+
+//     if (index ===3 ){
+//         item.innerHTML = `Peach
+//         <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//         </button>`;
+//     }
+// })
+
+// const listItems =  document.getElementsByClassName('item');
+// console.log(listItems[2].innerText);
+
+// const listItemsArray = Array.from(listItems);
+
+// listItemsArray.forEach((item) => {
+//     console.log(item.innerText);
+// })
+
+// const listItems3 =  document.getElementsByTagName('li');
+// console.log(listItems3[1].innerText);
