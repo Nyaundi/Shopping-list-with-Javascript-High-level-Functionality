5f102db87b2f9413413d8ee80732b02f36ea60d6
Nyaundi <jamesmabuka260@gmail.com>
2023-09-06 08:15:50 +0300

    Added DOM multiple elements and traversing the dom

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 //DOM Select Multiple Items -  use queryselectorall
 
-const listItems = document.querySelectorAll('.items');
+// const listItems = document.querySelectorAll('.item');
 
-console.log(listItems);
+// console.log(listItems[1].innerText);
+
+// listItems.forEach((a, index) => {
+//     a.style.color = 'red';
+
+//     if (index === 1){
+//         //a.remove();
+//     }
+//     if (index === 0){
+//         a.innerHTML = ` Mangoes
+//         <button class="remove-item btn-link text-red">
+//           <i class="fa-solid fa-xmark"></i>
+//         </button>`
+//     }
+// })
+
+// Small practce
+// const listUl = document.querySelectorAll('.item');
+
+// listUl.forEach((a, index) => {
+//     if (index === 0){
+//         a.innerHTML = `Juju
+//           <button class="remove-item btn-link text-red">
+//           <i class="fa-solid fa-xmark"></i>
+//         </button>`
+//         a.style.background = 'Orange'
+//     }
+// })
